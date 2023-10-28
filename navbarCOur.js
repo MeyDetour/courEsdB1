81e46247c72981787d6046d19b9ba8e20346a553
mey <meydetour@gmail.com>
2023-10-28 17:20:56 +0200

    s

---
+++ navbarCOur.js
@@ -0,0 +1,11 @@
+let navbar = document.querySelector('.navbarcour')
+
+navbar.innerHTML = `
+    <div class="navbarCour">
+        <div class="scroll-space">
+        </div>
+        <div class="iconeHome">
+            <a href="../index.html"> <i class="bi bi-house-door-fill"></i></a>
+        </div>
+    </div>
+`;
